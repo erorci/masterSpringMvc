420c110f45bde35d842207ac19fff6e239ed6bf1
Emerson <erorci@gmail.com>
2016-10-19 23:22:12 -0200

    Fim capítulo 3

--- src/main/java/masterSpringMvc/ProfileController.java
+++ src/main/java/masterSpringMvc/ProfileController.java
@@ -2,6 +2,7 @@ package masterSpringMvc;
 
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ProfileController {
-	
+
 	@RequestMapping("/profile")
 	public String displayProfile(ProfileForm profileForm) {
 		return "profile/profilePage";
@@ -30,5 +31,18 @@ public class ProfileController {
 		}
 		System.out.println("save ok" + profileForm);
 		return "redirect:/profile";
+	}
+
+	@RequestMapping(value = "/profile", params = { "addTaste" })
+	public String addRow(ProfileForm profileForm) {
+		profileForm.getTastes().add(null);
+		return "profile/profilePage";
+	}
+
+	@RequestMapping(value = "/profile", params = { "removeTaste" })
+	public String removeRow(ProfileForm profileForm, HttpServletRequest req) {
+		Integer rowId = Integer.valueOf(req.getParameter("removeTaste"));
+		profileForm.getTastes().remove(rowId.intValue());
+		return "profile/profilePage";
 	}
 }
